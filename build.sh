c1ff8654e3277c33cb307f8c8642cbf76aeddfc2
James Hibberd <james.d.hibberd@gmail.com>
2026-03-20 17:53:36 +0800

    chapter 5

--- build.sh
+++ build.sh
@@ -6,19 +6,34 @@ cd "$(dirname "$0")"
 # Create a build directory to keep class files separate from source
 mkdir -p build
 
-# Compile all Java files into the build directory
-# Since they have package 'com.craftinginterpreters.lox',
-# they will be placed in 'build/com/craftinginterpreters/lox/'
-javac -d build *.java
+# 1. Compile the tools
+echo "Compiling tools..."
+javac -d build tool/*.java
+if [ $? -ne 0 ]; then
+    echo "Tool compilation failed."
+    exit 1
+fi
+
+# 2. Run the tool to generate code
+echo "Generating AST..."
+java -cp build com.craftinginterpreters.tool.GenerateAst lox/
+if [ $? -ne 0 ]; then
+    echo "AST generation failed."
+    exit 1
+fi
+
+# 3. Compile all Java files (including generated ones) into the build directory
+echo "Compiling Lox..."
+javac -d build lox/*.java
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
-    echo "Compilation successful."
+    echo "Lox compilation successful."
     
     # If any arguments are provided, run the Lox class with them
     # Otherwise, start the REPL
     java -cp build com.craftinginterpreters.lox.Lox "$@"
 else
-    echo "Compilation failed."
+    echo "Lox compilation failed."
     exit 1
 fi
