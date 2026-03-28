04af54bf27f246d0e15e6e6055deb44804a47de4
James Hibberd <james.d.hibberd@gmail.com>
2026-03-28 16:40:59 +0800

    chapter 7

--- lox/Lox.java
+++ lox/Lox.java
@@ -9,8 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
-
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
   
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
@@ -27,6 +28,7 @@ public class Lox {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
     if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
   }
 
   // test sending ctrl-D to sent null to stdin
@@ -52,7 +54,8 @@ public class Lox {
     // Stop if there was a syntax error
     if (hadError) return;
 
-    System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(expression);
+    // System.out.println(new AstPrinter().print(expression));
   }
 
   static void error(int line, String message) {
@@ -72,5 +75,11 @@ public class Lox {
     } else {
       report(token.line, " at '" + token.lexeme + "'", message);
     }
+  }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+      "\n[line " + error.token.line + " ]");
+    hadRuntimeError = true;
   }
 }
